e4f2cb2ea931f4af46fbccd9065fbb026f7ce61b
Ethan Elmer <ethankelmer@gmail.com>
2025-11-08 15:58:10 -0600

    set up chroma connection

---
+++ BackEnd/chromaConnection.py
@@ -0,0 +1,7 @@
+import chromadb
+
+client = chromadb.CloudClient(
+  api_key='ck-AYaFfBmELHXwQcB4o4ZqPuECYjFG4Yv3cn86x9Ehsmye',
+  tenant='2ffaaf63-2457-413d-bace-23eab58c1b01',
+  database='TutorDatabase'
+)
